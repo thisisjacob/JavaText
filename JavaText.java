c5e09264778912f768d86e47d90ffd9011237fd0
jacob <42303925+thisisjacob@users.noreply.github.com>
2019-08-29 19:43:46 -0400

    TextArea functioning, now scrolls

--- JavaText.java
+++ JavaText.java
@@ -5,6 +5,7 @@ public class JavaText extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 400;
     private JFrame baseWindow;
+    private JScrollPane mainTextScrollPane;
     private JTextArea mainText;
     private JMenuBar menuBar;
     private JMenu file;
@@ -23,34 +24,34 @@ public class JavaText extends JFrame {
         super("JavaText");
 
         baseWindow = new JFrame();
-        mainText = new JTextArea(40, 40);
-        menuBar = new JMenuBar();
-        file = new JMenu("File");
-        save = new JMenuItem("Save");
-        load = new JMenuItem("Load");
-
-
         baseWindow.setSize(WIDTH, HEIGHT);
         baseWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         baseWindow.setLayout(new BorderLayout());
-
-        mainText.setBounds(0, 0, 400, 400);
-        mainText.append("testing testing one two three /n\n sfdadsafasdfsd\n\n\n dfasdds");
-
-
         
+
+        mainText = new JTextArea(20, 20);
+
+        mainTextScrollPane = new JScrollPane(mainText);
+        baseWindow.setLayout(new BorderLayout());
+        mainTextScrollPane.setBounds(0, 0 , 400, 400);
+        mainTextScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        mainTextScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        mainTextScrollPane.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+
+        menuBar = new JMenuBar();
+        file = new JMenu("File");
         menuBar.add(file);
         
+        save = new JMenuItem("Save");
+        load = new JMenuItem("Load");
         file.add(save);
         file.add(load);
 
 
 
         baseWindow.add(menuBar, BorderLayout.NORTH);
-        baseWindow.add(mainText, BorderLayout.CENTER);
+        baseWindow.add(mainTextScrollPane, BorderLayout.CENTER);
         baseWindow.setVisible(true);
-
-        
-
     }
 }
