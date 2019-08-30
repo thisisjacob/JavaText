3c75d639517cdef1437d1a817ab0fc3f35c4adc1
jacob <42303925+thisisjacob@users.noreply.github.com>
2019-08-30 09:00:40 -0400

    Now capable of saving text to a default file (javatextwrite)

--- JavaText.java
+++ JavaText.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import javax.swing.filechooser.*;
+import java.io.*;
+import java.io.FileWriter;
+import java.io.FileReader;
+
 
 public class JavaText extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 400;
+
+    private boolean isStreamOpen;
+
     private JFrame baseWindow;
     private JScrollPane mainTextScrollPane;
     private JTextArea mainText;
@@ -11,6 +20,9 @@ public class JavaText extends JFrame {
     private JMenu file;
     private JMenuItem save;
     private JMenuItem load;
+    //private JFileChooser fileChooser;
+    private FileWriter writeFile;
+    private FileReader readFile;
 
     public static void main(String[] args) {
         new JavaText();
@@ -23,12 +35,13 @@ public class JavaText extends JFrame {
     public JavaText() {
         super("JavaText");
 
-        baseWindow = new JFrame();
+        // creates main window of program
+        baseWindow = new JFrame(); 
         baseWindow.setSize(WIDTH, HEIGHT);
         baseWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         baseWindow.setLayout(new BorderLayout());
         
-
+        // text area nestled within a scroll pane
         mainText = new JTextArea(20, 20);
 
         mainTextScrollPane = new JScrollPane(mainText);
@@ -38,18 +51,63 @@ public class JavaText extends JFrame {
         mainTextScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         mainTextScrollPane.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 
-
+        // menu bar created, menu items added
         menuBar = new JMenuBar();
         file = new JMenu("File");
         menuBar.add(file);
         
         save = new JMenuItem("Save");
         load = new JMenuItem("Load");
+        //fileChooser = new JFileChooser();
+
+
+        // filereader created for reading items in a file to jtextarea
+        try {
+            readFile = new FileReader("javatextwrite");
+        }
+        catch(IOException exc) {
+            System.out.println("IO exception when creating new FileReader. Ending program.");
+            System.exit(0);
+        }
+
+        // creates new FileWriter object, writes text currently in mainText to a file named javatextwrite
+        // when save button is pressed
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                        writeFile = new FileWriter("javatextwrite", false);
+                        writeFile.write(mainText.getText());
+                        writeFile.flush();
+                        writeFile.close();
+                }
+                catch (IOException exc) {
+                    System.out.println(exc);
+                    System.exit(0);
+                }
+            }
+        });
+
+        // reads to file when save menu item is pressed
+        /** 
+        load.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                }
+                catch (IOException exc) {
+                    System.out.println("IO exception when writing to file. Ending program.");
+                    System.exit(0);
+                }
+            }
+        });*/
+        
+        // adds menu items to menu bar
+        //load.addActionListener(this);
         file.add(save);
         file.add(load);
 
 
-
+        // adds menubar and scrollpane to main window, makes visible
         baseWindow.add(menuBar, BorderLayout.NORTH);
         baseWindow.add(mainTextScrollPane, BorderLayout.CENTER);
         baseWindow.setVisible(true);
