0f881d01377bf0f641d75dcf7b1ed184e982008f
jacob <42303925+thisisjacob@users.noreply.github.com>
2019-09-12 19:29:19 -0400

    continued work on text appearance panel

--- JavaText.java
+++ JavaText.java
@@ -5,11 +5,12 @@ import javax.swing.filechooser.*;
 import java.io.*;
 import java.io.FileWriter;
 import java.io.FileReader;
+import java.awt.GraphicsEnvironment;
 
 
 public class JavaText extends JFrame {
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 400;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
 
     private JFrame baseWindow;
     private JScrollPane mainTextScrollPane;
@@ -26,6 +27,10 @@ public class JavaText extends JFrame {
     private ButtonGroup lineWrapGroup;
     private JRadioButtonMenuItem lineWrapTrue;
     private JRadioButtonMenuItem lineWrapFalse;
+    private JMenuItem openTextAppearanceMenu;
+
+    // loads available fonts on program run
+    private String[] availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(); 
 
 
 
@@ -38,7 +43,7 @@ public class JavaText extends JFrame {
         super("JavaText");
 
         // creates main window of program
-        baseWindow = new JFrame(); 
+        baseWindow = new JFrame("JavaText"); 
         baseWindow.setSize(WIDTH, HEIGHT);
         baseWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         baseWindow.setLayout(new BorderLayout());
@@ -103,6 +108,15 @@ public class JavaText extends JFrame {
         lineWrap.add(lineWrapTrue);
         lineWrap.add(lineWrapFalse);
         formatMenu.add(lineWrap);
+
+        openTextAppearanceMenu = new JMenuItem("Font...");
+        openTextAppearanceMenu.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                new TextAppearanceWindow();
+            }
+        });
+        formatMenu.add(openTextAppearanceMenu);
+
         menuBar.add(formatMenu);
 
 
@@ -154,6 +168,46 @@ public class JavaText extends JFrame {
         catch (IOException exc) {
             System.out.println("IO exception when writing to file. Ending program.");
             System.exit(0);
+        }
+    }
+
+    private class TextAppearanceWindow extends JFrame {
+        final int WIDTH = 400;
+        final int HEIGHT = 400;
+        
+
+        JFrame mainWindow;
+        JScrollPane fontSelectScroll;
+        JList fontSelect;
+        JPanel bottomButtonPanel;
+        JButton confirmSelections;
+        JButton cancelSelections;
+
+        private TextAppearanceWindow() {
+            super("Text Appearance Menu");
+
+            mainWindow = new JFrame("Text Appearance Menu");
+            mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mainWindow.setLayout(new BorderLayout());
+            mainWindow.setSize(WIDTH, HEIGHT);
+
+            
+            fontSelect = new JList(availableFonts);
+            fontSelectScroll = new JScrollPane();
+            fontSelectScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            fontSelectScroll.setPreferredSize(new Dimension(WIDTH / 3, HEIGHT - 50));
+            fontSelectScroll.add(fontSelect);
+
+            bottomButtonPanel = new JPanel();
+            confirmSelections = new JButton("Confirm");
+            cancelSelections = new JButton("Cancel");
+            bottomButtonPanel.add(confirmSelections);
+            bottomButtonPanel.add(cancelSelections);
+
+            mainWindow.add(fontSelectScroll, BorderLayout.WEST);
+            mainWindow.add(bottomButtonPanel, BorderLayout.SOUTH);
+        
+            mainWindow.setVisible(true);
         }
     }
 
