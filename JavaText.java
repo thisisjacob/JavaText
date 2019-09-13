e85026ce6ec49c55f0bcbe044c835c423a766422
jacob <42303925+thisisjacob@users.noreply.github.com>
2019-09-13 14:10:48 -0400

    basic font selection working

--- JavaText.java
+++ JavaText.java
@@ -5,11 +5,12 @@ import javax.swing.filechooser.*;
 import java.io.*;
 import java.io.FileWriter;
 import java.io.FileReader;
+import java.awt.GraphicsEnvironment;
 
 
 public class JavaText extends JFrame {
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 400;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 800;
 
     private JFrame baseWindow;
     private JScrollPane mainTextScrollPane;
@@ -27,6 +28,13 @@ public class JavaText extends JFrame {
     private JRadioButtonMenuItem lineWrapTrue;
     private JRadioButtonMenuItem lineWrapFalse;
     private JMenuItem openTextAppearanceMenu;
+
+    // loads available fonts on program run
+    private String[] availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(); 
+    
+    private String fontName;
+    private int fontStyle;
+    private int fontSize;
 
 
 
@@ -168,20 +176,62 @@ public class JavaText extends JFrame {
     }
 
     private class TextAppearanceWindow extends JFrame {
-        final int WIDTH = 100;
-        final int HEIGHT = 100;
+        final int WIDTH = 400;
+        final int HEIGHT = 400;
+        
+
         JFrame mainWindow;
+        JScrollPane fontSelectScroll;
+        JList<String> fontSelect;
+        JPanel bottomButtonPanel;
+        JButton confirmSelections;
+        JButton cancelSelections;
 
         private TextAppearanceWindow() {
             super("Text Appearance Menu");
 
             mainWindow = new JFrame("Text Appearance Menu");
-            mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            mainWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             mainWindow.setLayout(new BorderLayout());
             mainWindow.setSize(WIDTH, HEIGHT);
 
+            
+            fontSelect = new JList<String>(availableFonts);
+            fontSelect.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            fontSelect.setLayoutOrientation(JList.VERTICAL);
+            fontSelectScroll = new JScrollPane(fontSelect);
+            fontSelectScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+            fontSelectScroll.setPreferredSize(new Dimension(WIDTH / 3, HEIGHT - 50));
 
+            bottomButtonPanel = new JPanel();
+            confirmSelections = new JButton("Confirm");
+            confirmSelections.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    confirmButtonAction();
+                }
+            });
+            cancelSelections = new JButton("Cancel");
+            cancelSelections.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mainWindow.setVisible(false);
+                    mainWindow.dispose();
+                }
+            });
+            bottomButtonPanel.add(confirmSelections);
+            bottomButtonPanel.add(cancelSelections);
+
+            mainWindow.add(fontSelectScroll, BorderLayout.WEST);
+            mainWindow.add(bottomButtonPanel, BorderLayout.SOUTH);
+        
             mainWindow.setVisible(true);
+        }
+
+        private void confirmButtonAction() {
+            if (!fontSelect.isSelectionEmpty()) {
+                mainText.setFont(new Font(fontSelect.getSelectedValue(), Font.PLAIN, 11));
+            }
+            mainWindow.setVisible(false);
+            mainWindow.dispose();
         }
     }
 
